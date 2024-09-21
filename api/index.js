89a5918f5f3124a33282e68001ebf04a3003a83e
nooshin53 <rezaeinrt@gmail.com>
2024-09-21 22:34:51 +0330

    fix: changes cors poliy

---
+++ api/index.js
@@ -0,0 +1,43 @@
+// api/index.js
+import express from "express";
+import AllRoutes from "../routes/index.mjs"; // Adjust the path
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
+
+app.use(
+    cors({
+      origin: "https://web-backoffice-panel-amin-m75q.vercel.app", // No trailing slash
+      credentials: true,
+    })
+  );
+  
+
+app.use(
+  session({
+    secret: "PanelAdminSession",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: "Lax",
+      secure: false,
+      maxAge: 60000 * 60,
+    },
+  })
+);
+
+app.use(cookieParser("secret"));
+app.use(AllRoutes);
+
+// Export the app so Vercel can handle the serverless function
+export default app;
